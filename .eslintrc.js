5b828db9df4bd63e4171081a50a5aa2c73c4ffa9
marv <manchkhava.bozheva@gmail.com>
2025-03-15 23:53:00 +0300

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
+  stylistic.configs.recommended,
+  pluginJs.configs.recommended,
   {
     files: ['**/*.js'],
-    languageOptions: {
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-    parserOptions: {
-      sourceType: 'module',
-    },
+  },
+  {
+    ignores: ['dist/'],
   },
   {
     languageOptions: {
-      globals: globals.browser,
+      globals: globals.node,
+      parserOptions: {
+        sourceType: 'module',
+      },
     },
   },
-  pluginJs.configs.recommended,
 ];
