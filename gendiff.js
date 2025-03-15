25c4192219c708509ec4648c47e23e28d3210da0
marv <manchkhava.bozheva@gmail.com>
2025-03-15 21:21:36 +0300

    modified gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -2,6 +2,7 @@
 
 const { Command } = require('commander');
 const program = new Command();
+const parseFile = require('./src/parser');
 
 program
   .version('1.0.0')
@@ -9,4 +10,12 @@ program
   .argument('<filepath1>', 'first configuration file')
   .argument('<filepath2>', 'second configuration file')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const file1Data = parseFile(filepath1);
+    const file2Data = parseFile(filepath2);
+    
+    console.log('File 1 Data:', file1Data);
+    console.log('File 2 Data:', file2Data);
+    
+  })
   .parse(process.argv);
