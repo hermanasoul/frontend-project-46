df6ba618516aa50e17c83a010a6ac7dd7d7e9dfe
marv <manchkhava.bozheva@gmail.com>
2025-03-15 23:32:11 +0300

    import syntax change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
