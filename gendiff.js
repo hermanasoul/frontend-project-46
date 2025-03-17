7e0788d3cfa33caba4eb61b506dbf76c23e4a6b2
marv <manchkhava.bozheva@gmail.com>
2025-03-17 17:49:55 +0300

    modified gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander')
-const _ = require('lodash')
-const parseFile = require('./src/parser')
+const { Command } = require('commander');
+const _ = require('lodash');
+const { parseFile } = require('./src/parser');
 
 const genDiff = (filepath1, filepath2) => {
-  const file1Data = parseFile(filepath1)
-  const file2Data = parseFile(filepath2)
+  const file1Data = parseFile(filepath1);
+  const file2Data = parseFile(filepath2);
 
-  const sortedKeys = _.sortBy(_.union(_.keys(file1Data), _.keys(file2Data)))
+  const sortedKeys = _.sortBy(_.union(_.keys(file1Data), _.keys(file2Data)));
 
   const diff = sortedKeys.map((key) => {
     if (!_.has(file2Data, key)) {
-      return `  - ${key}: ${file1Data[key]}`
+      return `  - ${key}: ${file1Data[key]}`;
     }
     if (!_.has(file1Data, key)) {
-      return `  + ${key}: ${file2Data[key]}`
+      return `  + ${key}: ${file2Data[key]}`;
     }
     if (file1Data[key] !== file2Data[key]) {
       return [
         `  - ${key}: ${file1Data[key]}`,
         `  + ${key}: ${file2Data[key]}`,
-      ]
+      ];
     }
-    return null
-  }).filter(Boolean)
+    return null;
+  }).filter(Boolean);
 
-  return diff.flat().join('\n')
-}
+  return diff.flat().join('\n');
+};
 
-const program = new Command()
+const program = new Command();
 
 program
   .version('1.0.0')
@@ -38,7 +38,7 @@ program
   .argument('<filepath2>', 'second configuration file')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2)
-    console.log(result)
+    const result = genDiff(filepath1, filepath2);
+    console.log(result);
   })
-  .parse(process.argv)
+  .parse(process.argv);
