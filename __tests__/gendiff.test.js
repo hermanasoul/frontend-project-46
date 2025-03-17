aac4d89bb1088d8cc4f3e756f3b179e2b533639d
marv <manchkhava.bozheva@gmail.com>
2025-03-17 17:52:34 +0300

    create gendiff.test.js

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,21 @@
+import { describe, it, expect } from 'vitest';
+import { execSync } from 'child_process';
+
+describe('gendiff CLI with YAML files', () => {
+  it('should generate correct diff between two YAML files', () => {
+
+    const file1 = './test/fixtures/file1.yml';
+    const file2 = './test/fixtures/file2.yml';
+
+    const result = execSync(`node ./gendiff.js ${file1} ${file2}`).toString();
+    
+    expect(result).toBe(`  - timeout: 50
+  + timeout: 20
+  + verbose: true
+  - follow: false
+  + follow: true
+  - host: hexlet.io
+  + proxy: 123.234.53.22
+  `);
+  });
+});
