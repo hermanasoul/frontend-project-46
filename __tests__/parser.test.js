e7563243609c0665c46c5abd95f3f095d4452591
marv <manchkhava.bozheva@gmail.com>
2025-03-17 16:52:13 +0300

    update parser.test.js

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -6,15 +6,15 @@ const getFixturePath = (filename) => `${process.cwd()}/${filename}`;
 
 describe('parse', () => {
   test('correctly compares identical flat JSON files', () => {
-    const file1 = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
-    const file2 = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file1.json');
 
     expect(parse(file1, file2)).toBe('The files are identical.');
   });
 
   test('correctly compares different flat JSON files', () => {
-    const file1 = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
-    const file2 = fs.readFileSync(getFixturePath('file2.json'), 'utf-8');
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
 
     expect(parse(file1, file2)).toBe('Differences found.');
   });
