ca10049a06a102ac9679e479be99e6f515f0c19d
marv <manchkhava.bozheva@gmail.com>
2025-03-17 17:48:32 +0300

    updating parsing, adding support for yaml format

--- src/parser.js
+++ src/parser.js
@@ -1,24 +1,27 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml'); // Подключаем js-yaml
 
+// Функция для парсинга файлов
 const parseFile = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath)
+  const absolutePath = path.resolve(process.cwd(), filepath);
   try {
-    const data = fs.readFileSync(absolutePath, 'utf-8')
-    return JSON.parse(data)
+    const fileExtension = path.extname(filepath).toLowerCase();
+
+    let data;
+    if (fileExtension === '.json') {
+      data = fs.readFileSync(absolutePath, 'utf-8');
+      return JSON.parse(data);
+    } else if (fileExtension === '.yml' || fileExtension === '.yaml') {
+      data = fs.readFileSync(absolutePath, 'utf-8');
+      return yaml.load(data);
+    } else {
+      throw new Error(`Unsupported file format: ${fileExtension}`);
+    }
+  } catch (error) {
+    console.error(`Error reading or parsing file: ${filepath}`);
+    process.exit(1);
   }
-  catch {
-    console.error(`Error reading or parsing file: ${filepath}`)
-    process.exit(1)
-  }
-}
+};
 
-const parse = (file1, file2) => {
-  const obj1 = parseFile(file1)
-  const obj2 = parseFile(file2)
-
-  const isIdentical = JSON.stringify(obj1) === JSON.stringify(obj2)
-  return isIdentical ? 'The files are identical.' : 'Differences found.'
-}
-
-module.exports = { parse }
+module.exports = { parseFile };
